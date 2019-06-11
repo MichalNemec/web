5c19f4416c8956c85d98b9a7b8a796cdfcf6a191
Nexen <NexenWD@gmail.com>
2019-06-11 15:41:47 +0200

    web folder

---
+++ web/app/modules/admin/views/menu/_search.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\MenuSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="menu-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+        'options' => [
+            'data-pjax' => 1
+        ],
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'title') ?>
+
+    <?= $form->field($model, 'slug') ?>
+
+    <?= $form->field($model, 'text') ?>
+
+    <?= $form->field($model, 'parent_id') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('admin', 'Search'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton(Yii::t('admin', 'Reset'), ['class' => 'btn btn-outline-secondary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
